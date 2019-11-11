5897f7feeb92d71b5b432526e85dcba4d2fd5e82
yansong1221 <yansong1221@hotmail.com>
2019-11-11 16:27:30 +0800

    11

--- ServerEngine/ServerEngine.h
+++ ServerEngine/ServerEngine.h
@@ -1,13 +1,12 @@
 #pragma once
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 #pragma warning(disable:4251)
 #pragma warning(disable:4099)
 
-#ifdef BUILD_ENGINE_DLL
-#define ENGINE_API  __declspec(dllexport)
 #pragma pack(1)
-
 struct TCPHeader
 {
 	uint32_t	msgID;
@@ -15,19 +14,34 @@ struct TCPHeader
 };
 
 #define ENGINE_PING_MSGID				1
-
 #pragma pack()
-#else
-#define ENGINE_API __declspec(dllimport)
 
-#include "Connection.h"
-#include "EventDispatcher.h"
-#include "HttpClient.h"
-#include "MemoryStream.h"
-#include "Signal.h"
-#include "TCPServer.h"
-#include "ThreadPool.h"
-#include "Timer.h"
-#include "TCPClient.h"
-#include "Logger.h"
+#ifdef BUILD_ENGINE_DLL
+	#ifdef _WIN32
+		#define ENGINE_API  __declspec(dllexport)
+	#else
+		#define ENGINE_API
+	#endif
+		
+#else
+	#ifdef _WIN32
+		#define ENGINE_API __declspec(dllimport)
+	#else
+		#define ENGINE_API
+	#endif
+
+	#include "Connection.h"
+	#include "EventDispatcher.h"
+	#include "HttpClient.h"
+	#include "MemoryStream.h"
+	#include "Signal.h"
+	#include "TCPServer.h"
+	#include "ThreadPool.h"
+	#include "Timer.h"
+	#include "TCPClient.h"
+	#include "Logger.h"
+
 #endif
+
+
+
